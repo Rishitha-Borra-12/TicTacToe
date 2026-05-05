7474ff4d650c361393e08c5a922af27f8fb0781c
Rishitha-Borra-12 <rb2409@srmist.edu.in>
2026-05-05 21:25:36 +0530

    uc10

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,19 +1,21 @@
-public class TicTacToe {
+public public class TicTacToe {
 
     static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        board[0][0] = 'X';
-        board[0][1] = 'X';
-        board[0][2] = 'X';
-
-        System.out.println(hasWon('X'));
+        System.out.println(isDraw());
     }
 
-    static boolean hasWon(char symbol) {
-
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)
-                return true;
-
-            if (board[0][i
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+} {
+    
+}
